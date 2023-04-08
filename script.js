c366e028f32245963c52edf46f6f62c4f2103d80
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-04-08 12:54:44 +0800

    2nd commit

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let user = "name";
-console.log(user);
+// for in loop
+
+let user = {
+  name: "John",
+  age: 30,
+  isAdmin: true,
+};
+
+for (let key in user) {
+  console.log(key);
+  console.log(user[key]);
+}
